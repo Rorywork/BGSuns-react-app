00bab78866769ceec69ba5ecbe7b249ff447cf63
Rorywork <rorywork95@outlook.com>
2020-02-16 21:17:12 +0000

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="purple">
+      <div className="appBody">
+        <h1>This is my App</h1>
+      </div>
+    </div>
+  );
+};
+export default App;
