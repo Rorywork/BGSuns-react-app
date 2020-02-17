9337d067f4b9e788478ca959ac5096b1c8938cce
Rorywork <rorywork95@outlook.com>
2020-02-17 11:39:41 +0000

    Logic all working - need to move VideoList to rhs

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import youtube from "../apis/youtube.js";
 import VideoList from "./VideoList.js";
+import VideoDetail from "./VideoDetail";
 
 // import axios from "axios";
 // const KEY = "AIzaSyCLlO3WtgA0Yfl24chDEn8PEu41musEaOk";
 // channel url = "UCj8XvQLPxMey8LRa3mzE_1g";
 
 class App extends React.Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
 
   componentDidMount = async () => {
     const response = await youtube.get("/search", {
@@ -22,13 +23,22 @@ class App extends React.Component {
     });
 
     console.log(response);
+    this.setState({ videos: response.data.items });
+    console.log(this.state.videos);
+  };
+
+  onVideoSelect = video => {
+    this.setState({ selectedVideo: video });
   };
 
   render() {
     return (
-      <div>
-        <h1>This is my App</h1>
-        <VideoList />
+      <div className="ui container">
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={this.onVideoSelect}
+          videos={this.state.videos}
+        />
       </div>
     );
   }
