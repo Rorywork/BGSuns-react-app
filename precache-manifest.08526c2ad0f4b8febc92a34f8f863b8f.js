9b96e0e32c8c8b10e442474dc45ccfd4d6d4ca31
Rorywork <rorywork95@outlook.com>
2020-02-17 13:50:51 +0000

    Updates

---
+++ precache-manifest.08526c2ad0f4b8febc92a34f8f863b8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0602e46911e2436a479ba5cfdf753972",
+    "url": "/BGSuns-react-app/index.html"
+  },
+  {
+    "revision": "36a40dd46413d55a370e",
+    "url": "/BGSuns-react-app/static/css/main.73c1e589.chunk.css"
+  },
+  {
+    "revision": "aea12c9cfd8cbb127b39",
+    "url": "/BGSuns-react-app/static/js/2.12e18fb6.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/BGSuns-react-app/static/js/2.12e18fb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36a40dd46413d55a370e",
+    "url": "/BGSuns-react-app/static/js/main.6530c59a.chunk.js"
+  },
+  {
+    "revision": "cd654ba065e3e6ed9631",
+    "url": "/BGSuns-react-app/static/js/runtime-main.9713ae37.js"
+  }
+]);
